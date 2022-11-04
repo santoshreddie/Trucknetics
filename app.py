b9a5b698cface527a56d59debc06a6ece96cd817
Gondesi SivaRam Santosh Reddy <31606695+santoshreddie@users.noreply.github.com>
2022-11-04 11:12:17 +0530

    first repository

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request #importing the necessary libraries 
+import pickle
+import numpy as np
+app = Flask(__name__)
+
+
+@app.route('/', methods = ["GET", "POST"])# this code block is to show the user html page to enter the details to be predicted and the clicking the submit button
+def hello():
+    print("Request for index page received as it is")
+    return render_template('index2.html')
+
+@app.route('/predict', methods = ['POST']) #This code block is when the user clicked the submit button details from html page will be received using "request.form" and predictio
+                                           #will be done based on the array
+def prediction():
+    if request.method == 'POST':
+        product = request.form['product']
+        fro = request.form['fro']
+        to = request.form['to']
+        quantity = request.form['quantity']
+        permt = request.form['permt']
+        commo = request.form['commo']
+
+        data = [[product, fro, to, float(quantity), permt, commo]]
+        model = pickle.load(open('revenue.pkl', 'rb'))
+
+        prediction = np.array(model.predict(data)[0])
+
+    return render_template('result.html', n = prediction)# result will be posted on the prediction page
+
+
+if __name__ == '__main__': #main execution code
+    app.run()
